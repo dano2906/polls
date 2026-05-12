708d68ce4908d9b5cb16e81b1a0246a33ddc50dd
dano2906 <dlg9806@gmail.com>
2026-05-12 13:29:46 -0400

    chore(poll): Get poll details

--- src/actions/poll.ts
+++ src/actions/poll.ts
@@ -1,28 +1,64 @@
 import { randomUUID } from "node:crypto";
 import { createServerFn } from "@tanstack/react-start";
+import { asc, eq } from "drizzle-orm";
 import { poll } from "#/db/schema.ts";
 import { generateRandomCode } from "#/lib/utils.ts";
 import { createPollInput } from "#/shared/validation.ts";
 import { db } from "@/db";
-import { asc, eq } from "drizzle-orm";
 
-export const getUserPolls = createServerFn({method: 'GET'}).inputValidator((data: {userId:string})=>data).handler(async ({data}) => {
-	try {
-		const res =  await db.select({
-			name: poll.name,
-			description: poll.description,
-			slug: poll.slug,
-			startDate: poll.startDate,
-			endDate: poll.endDate,
-			status: poll.status,
-			version: poll.version
-		}).from(poll).where(eq(poll.userId,data.userId)).orderBy(asc(poll.startDate))
-		if(!res || res.length === 0) return []
-		return res
-	} catch (error) {
-		console.log('error',error)
-	}
-})
+export const getUserPolls = createServerFn({ method: "GET" })
+	.inputValidator((data: { userId: string }) => data)
+	.handler(async ({ data }) => {
+		try {
+			const res = await db
+				.select({
+					name: poll.name,
+					description: poll.description,
+					slug: poll.slug,
+					startDate: poll.startDate,
+					endDate: poll.endDate,
+					status: poll.status,
+					version: poll.version,
+				})
+				.from(poll)
+				.where(eq(poll.userId, data.userId))
+				.orderBy(asc(poll.startDate));
+			if (!res || res.length === 0) return [];
+			return res;
+		} catch (error) {
+			console.log("error", error);
+		}
+	});
+
+export const getPollDetails = createServerFn({ method: "GET" })
+	.inputValidator((data: { slug: string }) => data)
+	.handler(async ({ data }) => {
+		return await db.query.poll.findFirst({
+			where: (poll, { eq }) => eq(poll.slug, data.slug),
+			columns: {
+				description: true,
+				endDate: true,
+				name: true,
+				startDate: true,
+				status: true,
+				version: true,
+			},
+			with: {
+				pollQuestions: {
+					columns: {
+						order: true,
+					},
+					with: {
+						question: {
+							with: {
+								answers: true,
+							},
+						},
+					},
+				},
+			},
+		});
+	});
 
 export const createPoll = createServerFn({ method: "POST" })
 	.inputValidator((data) => createPollInput.parse(data))
--- src/routes/_protected/poll/update.$slug.tsx
+++ src/routes/_protected/poll/update.$slug.tsx
@@ -1,10 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { getPollDetails } from "#/actions/poll";
 
-export const Route = createFileRoute('/_protected/poll/update/$slug')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/_protected/poll/update/$slug")({
+	component: RouteComponent,
+	loader: async (ctx) => {
+		const { slug } = ctx.params;
+		return await getPollDetails({ data: { slug } });
+	},
+});
 
 function RouteComponent() {
-  const {slug} = Route.useParams()
-  return <div>Hello "/_protected/poll/update/$slug"! {slug}</div>
+	const data = Route.useLoaderData();
+	return (
+		<div>
+			<pre
+				style={{
+					padding: "1rem",
+					borderRadius: "8px",
+					overflowX: "auto",
+				}}
+			>
+				{JSON.stringify(data, null, 2)}
+			</pre>
+		</div>
+	);
 }
