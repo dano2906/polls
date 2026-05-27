08d26d25bc9ce17507737157acbd4da8728b3695
dano2906 <dlg9806@gmail.com>
2026-05-27 17:57:58 -0400

    feat(poll): Add CardContextMenu component for poll actions; refactor poll card components to utilize new context menu for improved UI interaction

--- src/components/partials/poll-card-dashboard-compact.tsx
+++ src/components/partials/poll-card-dashboard-compact.tsx
@@ -1,8 +1,5 @@
-import { Link } from "@tanstack/react-router";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
-import { Pencil } from "lucide-react";
-import { cn } from "#/lib/utils";
 import {
 	Accordion,
 	AccordionContent,
@@ -10,7 +7,6 @@ import {
 	AccordionTrigger,
 } from "../ui/accordion";
 import { Badge } from "../ui/badge";
-import { buttonVariants } from "../ui/button";
 import {
 	Card,
 	CardContent,
@@ -19,16 +15,8 @@ import {
 	CardHeader,
 	CardTitle,
 } from "../ui/card";
-import {
-	ContextMenu,
-	ContextMenuContent,
-	ContextMenuItem,
-	ContextMenuTrigger,
-} from "../ui/context-menu";
-import CopyClipboardPoll from "./copy-clipboard-poll";
-import DeletePollButton from "./delete-poll-button";
-import ForkVersionButton from "./fork-poll-button";
-import PollQrPopover from "./poll-qrcode-popover";
+
+import CardContextMenu from "./card-context-menu";
 
 interface Props {
 	pollGroup: {
@@ -49,170 +37,95 @@ const PollCardDashboardCompact = ({ pollGroup }: Props) => {
 	const lastPoll = pollGroup[0];
 	return (
 		<Card className="p-4 gap-3 w-full h-full flex flex-col items-center justify-between">
-			<ContextMenu>
-				<ContextMenuTrigger className="w-full h-full flex flex-col items-center justify-between">
-					<CardHeader className="w-full p-0.5 gap-1">
-						<CardTitle className="font-sgc text-2xl font-medium tracking-wide text-truncate line-clamp-2">
-							{lastPoll.name}
-						</CardTitle>
-						{/* Si no hay descripción, este componente no ocupará espacio */}
-						{lastPoll.description && (
-							<CardDescription className="font-sg text-truncate line-clamp-4">
-								{lastPoll.description}
-							</CardDescription>
-						)}
-					</CardHeader>
-
-					<CardContent className="w-full p-0.5 gap-1 flex-1 flex flex-col justify-end">
-						<div>
-							<Badge>{lastPoll.status}</Badge>
-							<Badge
-								variant={"secondary"}
-								className="text-xs font-sg font-thin"
-							>
-								<span className="text-accent-foreground text-xs">
-									{format(lastPoll.startDate, "dd/MM/yyyy", {
+			<CardContextMenu poll={lastPoll} forkVersion={lastPoll.version as number}>
+				<CardHeader className="w-full p-0.5 gap-1">
+					<CardTitle className="font-sgc text-2xl font-medium tracking-wide text-truncate line-clamp-2">
+						{lastPoll.name}
+					</CardTitle>
+					{lastPoll.description && (
+						<CardDescription className="font-sg text-truncate line-clamp-4">
+							{lastPoll.description}
+						</CardDescription>
+					)}
+				</CardHeader>
+				<CardContent className="w-full p-0.5 gap-1 flex-1 flex flex-col justify-end">
+					<div>
+						<Badge>{lastPoll.status}</Badge>
+						<Badge variant={"secondary"} className="text-xs font-sg font-thin">
+							<span className="text-accent-foreground text-xs">
+								{format(lastPoll.startDate, "dd/MM/yyyy", {
+									locale: es,
+								})}{" "}
+							</span>
+							{lastPoll.endDate && `...`}
+							{lastPoll.endDate && (
+								<span className="text-accent-foreground">
+									{" "}
+									{format(lastPoll.endDate, "dd/MM/yyyy", {
 										locale: es,
-									})}{" "}
+									}).toString()}
 								</span>
-								{lastPoll.endDate && `...`}
-								{lastPoll.endDate && (
-									<span className="text-accent-foreground">
-										{" "}
-										{format(lastPoll.endDate, "dd/MM/yyyy", {
-											locale: es,
-										}).toString()}
-									</span>
-								)}
-							</Badge>
-							<Badge
-								variant={"outline"}
-								className="font-sg text-xs font-semibold text-accent-foreground"
-							>
-								V{lastPoll.version}
-							</Badge>
-						</div>
-					</CardContent>
-				</ContextMenuTrigger>
-				<ContextMenuContent className="grid w-auto">
-					<ContextMenuItem asChild>
-						<Link
-							to="/poll/update/$slug"
-							params={{
-								slug: lastPoll.slug as string,
-							}}
-							preload={false}
-							className={cn(
-								buttonVariants({
-									variant: "ghostContext",
-								}),
-								"w-full flex items-center justify-start",
 							)}
+						</Badge>
+						<Badge
+							variant={"outline"}
+							className="font-sg text-xs font-semibold text-accent-foreground"
 						>
-							<Pencil />
-							Editar encuesta
-						</Link>
-					</ContextMenuItem>
-					<ContextMenuItem asChild>
-						<ForkVersionButton
-							slug={lastPoll.slug as string}
-							version={lastPoll.version as number}
-						/>
-					</ContextMenuItem>
-					<ContextMenuItem asChild>
-						<CopyClipboardPoll slug={lastPoll.slug as string} />
-					</ContextMenuItem>
-					<ContextMenuItem asChild>
-						<PollQrPopover slug={lastPoll.slug as string} />
-					</ContextMenuItem>
-					<ContextMenuItem asChild>
-						<DeletePollButton slug={lastPoll.slug as string} />
-					</ContextMenuItem>
-				</ContextMenuContent>
-			</ContextMenu>
+							V{lastPoll.version}
+						</Badge>
+					</div>
+				</CardContent>
+			</CardContextMenu>
 			<CardFooter className="px-1 w-full">
 				<Accordion type="single" collapsible className="w-full">
 					{pollGroup.slice(1).map((poll) => {
 						return (
-							<ContextMenu key={poll.slug}>
-								<ContextMenuTrigger className="w-full h-full flex flex-col items-center justify-between">
-									<AccordionItem
-										value={poll.slug as string}
-										className="w-full border-b-0 bg-muted/20"
-									>
-										<AccordionTrigger className="w-full px-4 hover:no-underline text-sm font-sg tracking-wide">
-											{`${poll.name} (v${poll.version})`}
-										</AccordionTrigger>
+							<CardContextMenu
+								key={poll.slug}
+								poll={poll}
+								forkVersion={lastPoll.version as number}
+							>
+								<AccordionItem
+									value={poll.slug as string}
+									className="w-full border-b-0 bg-muted/20"
+								>
+									<AccordionTrigger className="w-full px-4 hover:no-underline text-sm font-sg tracking-wide">
+										{`${poll.name} (v${poll.version})`}
+									</AccordionTrigger>
 
-										<AccordionContent className="px-4 pb-4 font-sg space-y-2">
-											<p>{poll.description}</p>
-											<div>
-												<Badge>{poll.status}</Badge>
-												<Badge
-													variant={"secondary"}
-													className="text-xs font-sg font-thin"
-												>
-													<span className="text-accent-foreground text-xs">
-														{format(poll.startDate, "dd/MM/yyyy", {
+									<AccordionContent className="px-4 pb-4 font-sg space-y-2">
+										<p>{poll.description}</p>
+										<div>
+											<Badge>{poll.status}</Badge>
+											<Badge
+												variant={"secondary"}
+												className="text-xs font-sg font-thin"
+											>
+												<span className="text-accent-foreground text-xs">
+													{format(poll.startDate, "dd/MM/yyyy", {
+														locale: es,
+													})}{" "}
+												</span>
+												{poll.endDate && `...`}
+												{poll.endDate && (
+													<span className="text-accent-foreground">
+														{" "}
+														{format(poll.endDate, "dd/MM/yyyy", {
 															locale: es,
-														})}{" "}
+														}).toString()}
 													</span>
-													{poll.endDate && `...`}
-													{poll.endDate && (
-														<span className="text-accent-foreground">
-															{" "}
-															{format(poll.endDate, "dd/MM/yyyy", {
-																locale: es,
-															}).toString()}
-														</span>
-													)}
-												</Badge>
-												<Badge
-													variant={"outline"}
-													className="font-sg text-xs font-semibold text-accent-foreground"
-												>
-													V{poll.version}
-												</Badge>
-											</div>
-										</AccordionContent>
-									</AccordionItem>
-								</ContextMenuTrigger>
-								<ContextMenuContent className="grid w-auto">
-									<ContextMenuItem asChild>
-										<Link
-											to="/poll/update/$slug"
-											params={{
-												slug: poll.slug as string,
-											}}
-											preload={false}
-											className={cn(
-												buttonVariants({
-													variant: "ghostContext",
-												}),
-												"w-full flex items-center justify-start",
-											)}
-										>
-											<Pencil />
-											Editar encuesta
-										</Link>
-									</ContextMenuItem>
-									<ContextMenuItem asChild>
-										<ForkVersionButton
-											slug={poll.slug as string}
-											version={lastPoll.version as number}
-										/>
-									</ContextMenuItem>
-									<ContextMenuItem asChild>
-										<CopyClipboardPoll slug={poll.slug as string} />
-									</ContextMenuItem>
-									<ContextMenuItem asChild>
-										<PollQrPopover slug={poll.slug as string} />
-									</ContextMenuItem>
-									<ContextMenuItem asChild>
-										<DeletePollButton slug={poll.slug as string} />
-									</ContextMenuItem>
-								</ContextMenuContent>
-							</ContextMenu>
+												)}
+											</Badge>
+											<Badge
+												variant={"outline"}
+												className="font-sg text-xs font-semibold text-accent-foreground"
+											>
+												V{poll.version}
+											</Badge>
+										</div>
+									</AccordionContent>
+								</AccordionItem>
+							</CardContextMenu>
 						);
 					})}
 				</Accordion>
--- src/components/partials/poll-card-dashboard-list.tsx
+++ src/components/partials/poll-card-dashboard-list.tsx
@@ -1,11 +1,6 @@
-import { Link } from "@tanstack/react-router";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
-import { Pencil } from "lucide-react";
-import { cn } from "#/lib/utils";
-import type { Poll } from "#/shared/types";
 import { Badge } from "../ui/badge";
-import { buttonVariants } from "../ui/button";
 import {
 	Card,
 	CardDescription,
@@ -13,104 +8,60 @@ import {
 	CardHeader,
 	CardTitle,
 } from "../ui/card";
-import {
-	ContextMenu,
-	ContextMenuContent,
-	ContextMenuItem,
-	ContextMenuTrigger,
-} from "../ui/context-menu";
-import CopyClipboardPoll from "./copy-clipboard-poll";
-import DeletePollButton from "./delete-poll-button";
-import ForkVersionButton from "./fork-poll-button";
-import PollQrPopover from "./poll-qrcode-popover";
+import CardContextMenu from "./card-context-menu";
 
 interface Props {
-	poll: Pick<
-		Poll,
-		| "name"
-		| "description"
-		| "slug"
-		| "startDate"
-		| "endDate"
-		| "version"
-		| "status"
-	>;
+	poll: {
+		id: string;
+		rootId: string | null;
+		name: string;
+		description: string | null;
+		slug: string | null;
+		startDate: Date;
+		endDate: Date | null;
+		status: "draft" | "published" | "archived" | null;
+		version: number | null;
+		createdAt: Date | null;
+	};
 }
 
 const PollCardDashoardList = ({ poll }: Props) => {
 	return (
-		<ContextMenu>
-			<ContextMenuTrigger>
-				<Card className="p-4 gap-3 w-full h-full flex flex-col items-center justify-between">
-					<CardHeader className="w-full p-0.5 gap-1">
-						<CardTitle className="font-sgc text-2xl font-medium tracking-wide text-truncate line-clamp-2">
-							{poll.name}
-						</CardTitle>
-						<CardDescription className="font-sg text-truncate line-clamp-4">
-							{poll.description}
-						</CardDescription>
-					</CardHeader>
-					<CardFooter className="w-full p-0.5 gap-1">
-						<Badge>{poll.status}</Badge>
-						<Badge variant={"secondary"} className="text-xs font-sg font-thin">
-							<span className="text-accent-foreground text-xs">
-								{format(poll.startDate, "dd/MM/yyyy", { locale: es })}{" "}
+		<CardContextMenu poll={poll} forkVersion={poll.version as number}>
+			<Card className="p-4 gap-3 w-full h-full flex flex-col items-center justify-between">
+				<CardHeader className="w-full p-0.5 gap-1">
+					<CardTitle className="font-sgc text-2xl font-medium tracking-wide text-truncate line-clamp-2">
+						{poll.name}
+					</CardTitle>
+					<CardDescription className="font-sg text-truncate line-clamp-4">
+						{poll.description}
+					</CardDescription>
+				</CardHeader>
+				<CardFooter className="w-full p-0.5 gap-1">
+					<Badge>{poll.status}</Badge>
+					<Badge variant={"secondary"} className="text-xs font-sg font-thin">
+						<span className="text-accent-foreground text-xs">
+							{format(poll.startDate, "dd/MM/yyyy", { locale: es })}{" "}
+						</span>
+						{poll.endDate && `...`}
+						{poll.endDate && (
+							<span className="text-accent-foreground">
+								{" "}
+								{format(poll.endDate, "dd/MM/yyyy", {
+									locale: es,
+								}).toString()}
 							</span>
-							{poll.endDate && `...`}
-							{poll.endDate && (
-								<span className="text-accent-foreground">
-									{" "}
-									{format(poll.endDate, "dd/MM/yyyy", {
-										locale: es,
-									}).toString()}
-								</span>
-							)}
-						</Badge>
-						<Badge
-							variant={"outline"}
-							className="font-sg text-xs font-semibold text-accent-foreground"
-						>
-							V{poll.version}
-						</Badge>
-					</CardFooter>
-				</Card>
-			</ContextMenuTrigger>
-			<ContextMenuContent className="grid w-auto">
-				<ContextMenuItem asChild>
-					<Link
-						to="/poll/update/$slug"
-						params={{
-							slug: poll.slug as string,
-						}}
-						preload={false}
-						className={cn(
-							buttonVariants({
-								variant: "ghostContext",
-							}),
-							"w-full flex items-center justify-start",
 						)}
+					</Badge>
+					<Badge
+						variant={"outline"}
+						className="font-sg text-xs font-semibold text-accent-foreground"
 					>
-						<Pencil />
-						Editar encuesta
-					</Link>
-				</ContextMenuItem>
-				<ContextMenuItem asChild>
-					<ForkVersionButton
-						slug={poll.slug as string}
-						version={poll.version as number}
-					/>
-				</ContextMenuItem>
-				<ContextMenuItem asChild>
-					<CopyClipboardPoll slug={poll.slug as string} />
-				</ContextMenuItem>
-				<ContextMenuItem asChild>
-					<PollQrPopover slug={poll.slug as string} />
-				</ContextMenuItem>
-				<ContextMenuItem asChild>
-					<DeletePollButton slug={poll.slug as string} />
-				</ContextMenuItem>
-			</ContextMenuContent>
-		</ContextMenu>
+						V{poll.version}
+					</Badge>
+				</CardFooter>
+			</Card>
+		</CardContextMenu>
 	);
 };
 
